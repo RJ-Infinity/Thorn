eb7055adad6b13ccb40fd72a517726e28e9a3133
RJ_Infinity <rjinfinity2004@gmail.com>
2022-01-30 21:08:40 +0000

    initial commit (contains the old version of Þ) mainly boilerplate

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Thorn Compiler
+
+## Aims for this project
+
+- [ ] Compiles to native code
+- [ ] Self Hosted
+- [ ] Turing Complete
+- [ ] relative conveience
+- [ ] cross platform
+- [ ] staticly typed with type checking
---
+++ src/cs/Token.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thorn
+{
+    public enum TokenType
+    {
+        VariableName,
+
+        ClassDeclaration,
+        FunctionDeclaration,
+        
+        OpenBrackets,
+        CloseBrackets,
+        
+        newClassOperator,
+        Assignment,
+        TypeDecleration,
+
+        StringLitteral,
+        NumberLitteral,
+    }
+    public class Token
+    {
+        public int LineNumber;
+        public int CharPos;
+        public TokenType Type;
+        public string Data;
+
+    }
+}
