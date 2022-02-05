4b4081ed9650f2029dd82bf528149d40a7ca3f15
RJ_Infinity <rjinfinity2004@gmail.com>
2022-02-05 18:05:54 +0000

    added a console front end to Þ

---
+++ src/cs/ThornFrontEnd/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thorn
+{
+    class Program
+    {
+        public void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Write("you need to provide a JSON config file\n");
+                System.Environment.Exit(1);
+            }
+            if (File.Exists(args[0]))
+            {
+
+            }
+        }
+    }
+}
