7425f3bb59586917e6b0e67930991866a33602dd
RJ_Infinity <rjinfinity2004@gmail.com>
2022-02-07 18:52:31 +0000

    improved Json File Debug Messages

--- src/cs/ThornFrontEnd/Program.cs
+++ src/cs/ThornFrontEnd/Program.cs
@@ -11,13 +11,17 @@ namespace Thorn
 {
     class Program
     {
-        public static void Main(string[] args)
+        public static void Assert(bool statement, string message)
         {
-            if (args.Length == 0 || !File.Exists(args[0]))
+            if (statement)
             {
-                Console.Error.Write("You need to provide a JSON config file\n");
+                Console.Error.WriteLine(message);
                 Environment.Exit(1);
             }
+        }
+        public static void Main(string[] args)
+        {
+            Assert(args.Length == 0 || !File.Exists(args[0]),"You need to provide a JSON config file");
             JSONType obj;
             try
             {
@@ -30,26 +34,24 @@ namespace Thorn
                 Environment.Exit(1);
                 return; //so the compiler dosent complain
             }
-            if (
-                obj.Type != JSON.Types.DICT ||
-                !obj.DictData.ContainsKey("Files") ||
-                obj["Files"].Type != JSON.Types.LIST ||
-                obj["Files"].ListData.Count > 0 ||
+            Assert(obj.Type != JSON.Types.DICT,"The root of the Json file must be a Dict");
+            Assert(!obj.DictData.ContainsKey("Files"),"The Json file must contain a files list");
+            Assert(obj["Files"].Type != JSON.Types.LIST,"The Json file must contain a files list");
+            Assert(obj["Files"].ListData.Count == 0,"The files list must contain files");
+            Assert(
+                !obj.DictData.ContainsKey("EntryPoint") && //it doesnt contain an entry point and...
                 (
-                    !obj.DictData.ContainsKey("EntryPoint") && //it doesnt contain an entry point and...
+                    !obj.DictData.ContainsKey("OutputType") ||// ... it doesetn contain an output type or ...
                     (
-                        !obj.DictData.ContainsKey("OutputType") ||// ... it doesetn contain an output type or ...
-                        (
-                            obj["OutputType"].Type != JSON.Types.STRING &&
-                            obj["OutputType"].StringData != "Libary"// ... the output type isnt a libary
-                        )
+                        obj["OutputType"].Type != JSON.Types.STRING &&
+                        obj["OutputType"].StringData != "Libary"// ... the output type isnt a libary
                     )
-                )
-
-            )
+                ),
+                "The Json file must either contain an entry point or have an output type of \"Libary\""
+            );
+            foreach(JSONType file in obj["Files"])
             {
-                Console.Error.Write("The Json file has the wrong format\n");
-                Environment.Exit(1);
+                Assert(file.Type == JSON.Types.STRING, "The Json files must be a string");
             }
         }
     }
