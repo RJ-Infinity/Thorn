a8398e82518f1615833ee81848616ebb00e429f9
RJ_Infinity <rjinfinity2004@gmail.com>
2022-02-14 21:25:23 +0000

    tree structure added

---
+++ src/cs/Tree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thorn
+{
+    class Tree<T>
+    {
+        private List<Tree<T>> childNodes;
+        public T Data;
+        public Tree(T data, List<Tree<T>> initalChildren)
+        {
+            Data = data;
+            childNodes = initalChildren;
+        }
+        public Tree(T data)
+        {
+            Data = data;
+            childNodes = new List<Tree<T>>();
+        }
+        public Tree<T> this[int i]
+        {
+            get => childNodes[i];
+        }
+    }
+}
