e2374431bb83b1450042023d36323a6df5b52ef0
RJ_Infinity <rjinfinity2004@gmail.com>
2022-03-05 16:56:22 +0000

    Renamed Tree to Graph

--- src/cs/Graph.cs
+++ src/cs/Graph.cs
@@ -7,27 +7,27 @@ using System.Threading.Tasks;
 
 namespace Thorn
 {
-    public class Tree<T> : IEnumerable<Tree<T>>
+    public class Graph<T> : IEnumerable<Graph<T>>
     {
-        private List<Tree<T>> childNodes;
+        private List<Graph<T>> childNodes;
         public T Data;
-        public Tree(T data, List<Tree<T>> initalChildren)
+        public Graph(T data, List<Graph<T>> initalChildren)
         {
             Data = data;
             childNodes = initalChildren;
         }
-        public Tree(T data)
+        public Graph(T data)
         {
             Data = data;
-            childNodes = new List<Tree<T>>();
+            childNodes = new List<Graph<T>>();
         }
-        public Tree<T> this[int i]
+        public Graph<T> this[int i]
         {
             get => childNodes[i];
         }
 
-        public IEnumerator<Tree<T>> GetEnumerator() => childNodes.GetEnumerator();
+        public IEnumerator<Graph<T>> GetEnumerator() => childNodes.GetEnumerator();
         IEnumerator IEnumerable.GetEnumerator() => childNodes.GetEnumerator();
-        public void Add(Tree<T> subTree) => childNodes.Add(subTree);
+        public void Add(Graph<T> subTree) => childNodes.Add(subTree);
     }
 }
