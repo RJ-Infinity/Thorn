e88c40b4cd70fe1a2d4d22f6c8853c2c23fae357
RJ_Infinity <rjinfinity2004@gmail.com>
2022-03-06 13:18:47 +0000

    changed graph significantly

--- src/cs/Graph.cs
+++ src/cs/Graph.cs
@@ -5,29 +5,72 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Thorn
+namespace Graph
 {
-    public class Graph<T> : IEnumerable<Graph<T>>
+    public class NamedGraph<T>
     {
-        private List<Graph<T>> childNodes;
+        public NamedGraph(string rootNode, T data, List<string> children)
+        {
+            root = rootNode;
+            Add(rootNode, data, children);
+        }
+        internal Dictionary<string, Node<T>> nodes = new Dictionary<string, Node<T>>();
+        private string root;
+        public void Add(string index, T data, List<string> children) => nodes.Add(index, new Node<T>(this, index, data, children));
+        public Node<T> this[int i]
+        {
+            get
+            {
+                return nodes[root][i];
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return root;
+            }
+            set
+            {
+                Node<T> temp = nodes[root];
+                nodes[value] = temp;
+                nodes[value].Name = value;
+                root = value;
+            }
+        }
+        public T Data
+        {
+            get
+            {
+                return nodes[root].Data;
+            }
+            set
+            {
+                nodes[root].Data = value;
+            }
+        }
+        public void ChangeRoot(string newRoot)
+        {
+            root = newRoot;
+        }
+    }
+    public class Node<T>
+    {
+        private NamedGraph<T> Parent;
+        public string Name;
         public T Data;
-        public Graph(T data, List<Graph<T>> initalChildren)
-        {
-            Data = data;
-            childNodes = initalChildren;
-        }
-        public Graph(T data)
-        {
-            Data = data;
-            childNodes = new List<Graph<T>>();
-        }
-        public Graph<T> this[int i]
-        {
-            get => childNodes[i];
-        }
+        private List<string> Children;
 
-        public IEnumerator<Graph<T>> GetEnumerator() => childNodes.GetEnumerator();
-        IEnumerator IEnumerable.GetEnumerator() => childNodes.GetEnumerator();
-        public void Add(Graph<T> subTree) => childNodes.Add(subTree);
+        internal Node(NamedGraph<T> parent, string name, T data, List<string> children)
+        {
+            Parent = parent;
+            Name = name;
+            Data = data;
+            Children = children;
+        }
+        public Node<T> this[int i]
+        {
+            get => Parent.nodes[Children[i]];
+        }
     }
 }
